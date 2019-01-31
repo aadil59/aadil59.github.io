9d37f2c708162b633b2d703e4c236cb6f689a702
Aadil Nawaz Khan (Tech- New Delhi HT House) <R180915@htmedia.in>
2019-01-31 15:10:01 +0530

    Updates

---
+++ precache-manifest.9bb9f02ec7790c72fd80814096e8473c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e10240f64f6dd1fe8b9",
+    "url": "/nadeem/static/js/runtime~main.9e10240f.js"
+  },
+  {
+    "revision": "741d661af18d2d3bf589",
+    "url": "/nadeem/static/js/main.741d661a.chunk.js"
+  },
+  {
+    "revision": "243fca57d77e31d69b78",
+    "url": "/nadeem/static/js/1.243fca57.chunk.js"
+  },
+  {
+    "revision": "741d661af18d2d3bf589",
+    "url": "/nadeem/static/css/main.3dde564e.chunk.css"
+  },
+  {
+    "revision": "243fca57d77e31d69b78",
+    "url": "/nadeem/static/css/1.6ba4f3a7.chunk.css"
+  },
+  {
+    "revision": "b1a8e1a278f1a0efa7bed63e0bb09f29",
+    "url": "/nadeem/index.html"
+  }
+];
